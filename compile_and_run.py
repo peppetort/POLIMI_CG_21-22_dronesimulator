135130af7b5dbdf355979191225723ccdeba101d
Giuseppe Tortorelli <peppetort@gmail.com>
2022-06-13 20:32:12 +0200

    python script to compile and run using cmake

---
+++ compile_and_run.py
@@ -0,0 +1,13 @@
+import os
+
+BUILD_FOLDER_RELATIVE_PATH = 'build'
+COMPILED_OUTPUT_NAME = 'CG_project'
+
+def main():
+    if not os.path.exists(BUILD_FOLDER_RELATIVE_PATH):
+        os.mkdir(BUILD_FOLDER_RELATIVE_PATH)
+
+    os.system(f'cd {BUILD_FOLDER_RELATIVE_PATH} && cmake .. && make && cd .. && ./{BUILD_FOLDER_RELATIVE_PATH}/{COMPILED_OUTPUT_NAME}')
+
+if __name__== "__main__":
+   main()
